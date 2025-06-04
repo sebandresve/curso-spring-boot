fa8fae67094d6f27ced1a83f69e2ef3146e722f0
sebandresve <sebandres.ve@gmail.com>
2025-06-04 11:53:33 -0400

    Api Rest enviar json petición POST

--- src/main/java/com/sebandresve/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/sebandresve/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -1,11 +1,17 @@
 package com.sebandresve.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sebandresve.curso.springboot.webapp.springboot_web.dto.ParamDto;
+import com.sebandresve.curso.springboot.webapp.springboot_web.models.User;
 
 @RestController
 @RequestMapping("/api/var")
@@ -18,4 +24,18 @@ public class PathVariableController {
         return params;
     }
 
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id) {
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id);
+        return json;
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        // hacer algo con el usuario save en la bbdd
+        user.setName(user.getName().toUpperCase());
+        return user;        
+    }
 }
